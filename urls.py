c2c849b01d59b5b7ecded88e9ab61010d1fc81e7
Joel Haasnoot <joelhaasnoot@gmail.com>
2012-11-05 22:13:04 +0100

    Add flatpages

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-	
+	('^over/', include('django.contrib.flatpages.urls')),
 	(r'', include('helpdesk.urls')),
 )
 
