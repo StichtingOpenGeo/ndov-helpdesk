35c22a1ab636bf15c8e422338b373817561a1d57
Joel Haasnoot <joelhaasnoot@gmail.com>
2012-11-06 20:41:47 +0100

    Fix URLs

--- urls.py
+++ urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^helpdesk/', include('helpdesk.urls')),
     ('^', include('django.contrib.flatpages.urls')),
-    (r'^helpdesk/', include('helpdesk.urls')),
 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
