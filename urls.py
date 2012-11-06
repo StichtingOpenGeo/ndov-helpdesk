65019e149880913bcb71a2eb9f6ea3988670dd54
Joel Haasnoot <joelhaasnoot@gmail.com>
2012-11-06 20:29:48 +0100

    Add template for flatpage

--- urls.py
+++ urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     ('^', include('django.contrib.flatpages.urls')),
-    (r'', include('helpdesk.urls')),
+    (r'^helpdesk/', include('helpdesk.urls')),
 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
