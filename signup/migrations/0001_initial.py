8a215f269d7a72818576a0f2109de0492d361316
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-01-05 18:31:57 +0100

    Cleanup migrations and write translations

--- signup/migrations/0001_initial.py
+++ signup/migrations/0001_initial.py
@@ -11,16 +11,16 @@ class Migration(SchemaMigration):
         # Adding model 'SignupQueue'
         db.create_table('signup_signupqueue', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=75)),
             ('representative', self.gf('django.db.models.fields.CharField')(max_length=75)),
             ('position', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
             ('business', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('city', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('signed_file', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
-            ('status', self.gf('django.db.models.fields.IntegerField')()),
-            ('date_requested', self.gf('django.db.models.fields.DateField')()),
-            ('date_uploaded', self.gf('django.db.models.fields.DateField')()),
-            ('date_verified', self.gf('django.db.models.fields.DateField')(blank=True)),
+            ('status', self.gf('django.db.models.fields.IntegerField')(default=1)),
+            ('date_requested', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
+            ('date_uploaded', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
+            ('date_verified', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
         ))
         db.send_create_signal('signup', ['SignupQueue'])
 
@@ -36,15 +36,15 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'SignupQueue'},
             'business': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'city': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'date_requested': ('django.db.models.fields.DateField', [], {}),
-            'date_uploaded': ('django.db.models.fields.DateField', [], {}),
-            'date_verified': ('django.db.models.fields.DateField', [], {'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'date_requested': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_uploaded': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'date_verified': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'position': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'representative': ('django.db.models.fields.CharField', [], {'max_length': '75'}),
             'signed_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
-            'status': ('django.db.models.fields.IntegerField', [], {})
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1'})
         }
     }
 
--- signup/models.py
+++ signup/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _, ugettext
 from django.conf import settings
 
-# Create your models here.
 class SignupQueue(models.Model):
     # Data for generating signup
+    email = models.EmailField(unique=True)
     representative = models.CharField(_('Name of representative'), max_length=75) 
     position = models.CharField(_('Position'), max_length=100)
-    email = models.EmailField()
     business = models.CharField(_('Organization name'), max_length=100)
     city = models.CharField(_('Signing city'), max_length=50)
     
