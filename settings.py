989a0816a9fb63b738efecdc8f6959d95ae9403b
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-02-09 18:56:56 +0100

    Add log formatter
    That should complete the formatting setup

--- settings.py
+++ settings.py
@@ -122,6 +122,12 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
