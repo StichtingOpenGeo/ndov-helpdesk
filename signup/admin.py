5d504f95f63f9f23b25e460e06fb41099c88beaf
Joel Haasnoot <joelhaasnoot@gmail.com>
2013-05-25 20:39:17 +0200

    Add status to signup overview

--- signup/admin.py
+++ signup/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from signup.models import SignupQueue
 
 class QueueAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position', 'organization', 'email', 'city')
+    list_display = ('name', 'position', 'organization', 'email', 'status')
 
 admin.site.register(SignupQueue, QueueAdmin)
